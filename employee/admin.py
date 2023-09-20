014bce54d7ba878e46340dfeec7622273902605d
iramfatima <iramfatimaclecotech@gmail.com>
2023-09-20 16:33:11 +0530

    add admin with model

--- employee/admin.py
+++ employee/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Employee
+admin.site.register(Employee)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('eid', 'ename', 'eemail', 'econtact')
+    list_filter = ('eid', 'econtact')  
+    search_fields = ('ename', 'eemail')  
+    date_hierarchy = 'join_date'
+    class Meta:
+        verbose_name = 'Custom Employee Name'
+        verbose_name_plural = 'Custom Employee Names'
